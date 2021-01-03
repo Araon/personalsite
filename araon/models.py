2ae4cfa952f1a39279d6dcb04be45eceb104ad1c
Araon <soumik.ghosh1998@gmail.com>
2021-01-03 22:02:45 +0530

    made changes to database schema

--- araon/models.py
+++ araon/models.py
@@ -15,6 +15,7 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(500), nullable=False)
+    sub_title = db.Column(db.String(500), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
